6ef015c722ea796c7099d3272b9f0cc1f4244081
ParikshitHoley <parikshitholey20@gmail.com>
2023-01-21 12:55:05 +0530

    jhgh

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const { connection } = require("./config/db.js")
+const { userRouter } = require("./routes/user.route")
+
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+// routes
+app.use("/user",userRouter)
+
+
+
+const port = process.env.PORT || 3006
+
+
+
+app.listen(port, () => {
+   connection()
+       console.log(`listening to port ${port}`);
+})
